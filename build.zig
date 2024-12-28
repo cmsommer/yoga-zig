9056f69b434a856fa450006bfbda7d132dea0244
Christiaan Sommer <cmsommer.proton.me>
2024-12-28 15:47:34 +0100

    use zig pm for yoga dep

--- build.zig
+++ build.zig
@@ -7,19 +7,53 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.addModule("zig-yoga", .{
+    const yoga_dep = b.dependency("yoga", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const yoga_zig_lib = b.addStaticLibrary(.{
+        .name = "zig-yoga",
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const yoga_zig_mod = b.addModule("zig-yoga", .{
         .target = target,
         .optimize = optimize,
         .link_libc = true,
         .link_libcpp = true,
     });
 
+    yoga_zig_lib.installHeadersDirectory(yoga_dep.path("yoga"), "yoga", .{
+        .include_extensions = &.{".h"},
+    });
+    yoga_zig_lib.linkLibC();
+    yoga_zig_lib.linkLibCpp();
+
     const lib: []const u8 = if (target.result.os.tag == .windows and target.result.abi == .msvc) "yogacore.lib" else "libyogacore.a";
     const lib_path = std.fmt.allocPrint(b.allocator, "./build/{s}", .{
         lib,
     }) catch unreachable;
-    mod.addIncludePath(b.path("yoga"));
-    mod.addAssemblyFile(b.path(lib_path));
+
+    const yoga_cmake = std.fmt.allocPrint(b.allocator, "cmake {s} -B {s} -G=Ninja", .{
+        yoga_dep.path(""),
+        yoga_dep.path("build"),
+    }) catch unreachable;
+
+    const yoga_compile_run = b.addSystemCommand(&.{
+        "CC='zig cc'",
+        "CXX='zig c++'",
+        yoga_cmake,
+    });
+    _ = yoga_compile_run; // autofix
+
+    yoga_zig_mod.addIncludePath(yoga_dep.path(""));
+    yoga_zig_mod.addAssemblyFile(b.path(lib_path));
+    yoga_zig_mod.linkLibrary(yoga_zig_lib);
+
+    b.installArtifact(yoga_zig_lib);
 
     const exe = b.addExecutable(.{
         .name = "zig-yoga",
@@ -28,8 +62,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    exe.addIncludePath(b.path("yoga"));
-    exe.root_module.addImport("zig-yoga", mod);
+    exe.linkLibrary(yoga_zig_lib);
 
     b.installArtifact(exe);
 
---
+++ src/root.zig
@@ -0,0 +1,1 @@
+pub usingnamespace @import("./c_api.zig");
